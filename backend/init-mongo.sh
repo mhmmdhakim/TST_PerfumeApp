73487232eef725f56eebadd3e53a992442bae214
mhmmdhkm <mhmmd.hkm25@gmail.com>
2025-01-09 01:34:37 +0700

    add start.sh

--- backend/init-mongo.sh
+++ backend/init-mongo.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
-
 echo "Starting MongoDB restore process..."
 
-# Mencoba koneksi ke MongoDB (menggunakan mongosh atau mongorestore) hingga berhasil
-RETRIES=5
-while [ $RETRIES -gt 0 ]; do
-    # Coba melakukan koneksi ke MongoDB
-    mongosh --eval "db.runCommand({ connectionStatus: 1 })" $MONGODB_URL && break
-    echo "MongoDB not ready yet. Retrying..."
-    RETRIES=$((RETRIES-1))
-    sleep 5
-done
-
-if [ $RETRIES -eq 0 ]; then
-    echo "MongoDB not ready after several attempts, exiting."
-    exit 1
-fi
-
-# Melakukan restore data menggunakan mongorestore
+# Gunakan environment variable dari Railway
 mongorestore \
     --uri="$MONGODB_URL" \
     --nsInclude="perfume_db.*" \
---
+++ backend/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./init-mongo.sh
+uvicorn app.main:app --host 0.0.0.0 --port $PORT
