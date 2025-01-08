562fdefcdeb42cd517f577c52833fb87d10fa1ba
mhmmdhkm <mhmmd.hkm25@gmail.com>
2025-01-09 01:23:25 +0700

    fix dockerfile 3

--- backend/init-mongo.sh
+++ backend/init-mongo.sh
@@ -2,7 +2,22 @@
 
 echo "Starting MongoDB restore process..."
 
-# Gunakan environment variable dari Railway
+# Mencoba koneksi ke MongoDB (menggunakan mongosh atau mongorestore) hingga berhasil
+RETRIES=5
+while [ $RETRIES -gt 0 ]; do
+    # Coba melakukan koneksi ke MongoDB
+    mongosh --eval "db.runCommand({ connectionStatus: 1 })" $MONGODB_URL && break
+    echo "MongoDB not ready yet. Retrying..."
+    RETRIES=$((RETRIES-1))
+    sleep 5
+done
+
+if [ $RETRIES -eq 0 ]; then
+    echo "MongoDB not ready after several attempts, exiting."
+    exit 1
+fi
+
+# Melakukan restore data menggunakan mongorestore
 mongorestore \
     --uri="$MONGODB_URL" \
     --nsInclude="perfume_db.*" \
